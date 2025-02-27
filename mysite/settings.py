5bc8cd344e64c756919067cc8ac84653faf2d29d
Nikodem <nikodem2287@gmail.com>
2025-02-27 20:08:29 +0100

    proba render 9

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,6 +97,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+# Media files (User-uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
